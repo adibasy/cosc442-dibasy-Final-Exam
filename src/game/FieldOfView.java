bc83562ce8929f23cb4bf6cecba2f1f283848291
Abdulai <adibas1@students.towson.edu>
2018-05-18 21:31:22 -0400

    Improved The overall quality of the code

--- src/game/FieldOfView.java
+++ src/game/FieldOfView.java
@@ -1,24 +1,66 @@
 package game;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class FieldOfView.
+ */
 public class FieldOfView {
+	
+	/** The world. */
 	private World world;
+	
+	/** The depth. */
 	private int depth;
 	
+	/** The visible. */
 	private boolean[][] visible;
+	
+	/**
+	 * Checks if is visible.
+	 *
+	 * @param x the x
+	 * @param y the y
+	 * @param z the z
+	 * @return true, if is visible
+	 */
 	public boolean isVisible(int x, int y, int z){
 		return z == depth && x >= 0 && y >= 0 && x < visible.length && y < visible[0].length && visible[x][y];
 	}
 	
+	/** The tiles. */
 	private Tile[][][] tiles;
+	
+	/**
+	 * Tile.
+	 *
+	 * @param x the x
+	 * @param y the y
+	 * @param z the z
+	 * @return the tile
+	 */
 	public Tile tile(int x, int y, int z){
 		return tiles[x][y][z];
 	}
 	
+	/**
+	 * Instantiates a new field of view.
+	 *
+	 * @param world the world
+	 */
 	public FieldOfView(World world){
 		this.world = world;
 		this.visible = new boolean[world.width()][world.height()];
 		this.tiles = new Tile[world.width()][world.height()][world.depth()];
 		
+		loopFor(world);
+	}
+
+	/**
+	 * Loop for.
+	 *
+	 * @param world the world
+	 */
+	private void loopFor(World world) {
 		for (int x = 0; x < world.width(); x++){
 			for (int y = 0; y < world.height(); y++){
 				for (int z = 0; z < world.depth(); z++){
@@ -28,27 +70,61 @@ public class FieldOfView {
 		}
 	}
 	
+	/**
+	 * Update.
+	 *
+	 * @param wx the wx
+	 * @param wy the wy
+	 * @param wz the wz
+	 * @param r the r
+	 */
 	public void update(int wx, int wy, int wz, int r){
 		depth = wz;
 		visible = new boolean[world.width()][world.height()];
 		
 		for (int x = -r; x < r; x++){
-			for (int y = -r; y < r; y++){
-				if (x*x + y*y > r*r)
-					continue;
-				
-				if (wx + x < 0 || wx + x >= world.width() || wy + y < 0 || wy + y >= world.height())
-					continue;
-				
-				for (Point p : new Line(wx, wy, wx + x, wy + y)){
-					Tile tile = world.tile(p.x, p.y, wz);
-					visible[p.x][p.y] = true;
-					tiles[p.x][p.y][wz] = tile; 
-					
-					if (!tile.isGround())
-						break;
-				}
-			}
+			loopFor2(wx, wy, wz, r, x);
+		}
+	}
+
+	/**
+	 * Loop for 2.
+	 *
+	 * @param wx the wx
+	 * @param wy the wy
+	 * @param wz the wz
+	 * @param r the r
+	 * @param x the x
+	 */
+	private void loopFor2(int wx, int wy, int wz, int r, int x) {
+		for (int y = -r; y < r; y++){
+			if (x*x + y*y > r*r)
+				continue;
+			
+			if (wx + x < 0 || wx + x >= world.width() || wy + y < 0 || wy + y >= world.height())
+				continue;
+			
+			loopFor1(wx, wy, wz, x, y);
+		}
+	}
+
+	/**
+	 * Loop for 1.
+	 *
+	 * @param wx the wx
+	 * @param wy the wy
+	 * @param wz the wz
+	 * @param x the x
+	 * @param y the y
+	 */
+	private void loopFor1(int wx, int wy, int wz, int x, int y) {
+		for (Point p : new Line(wx, wy, wx + x, wy + y)){
+			Tile tile = world.tile(p.x, p.y, wz);
+			visible[p.x][p.y] = true;
+			tiles[p.x][p.y][wz] = tile; 
+			
+			if (!tile.isGround())
+				break;
 		}
 	}
 }
--- src/game/WorldBuilder.java
+++ src/game/WorldBuilder.java
@@ -4,14 +4,37 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+// TODO: Auto-generated Javadoc
+/**
+ * The Class WorldBuilder.
+ */
 public class WorldBuilder {
+	
+	/** The width. */
 	private int width;
+	
+	/** The height. */
 	private int height;
+	
+	/** The depth. */
 	private int depth;
+	
+	/** The tiles. */
 	private Tile[][][] tiles;
+	
+	/** The regions. */
 	private int[][][] regions;
+	
+	/** The next region. */
 	private int nextRegion;
 
+	/**
+	 * Instantiates a new world builder.
+	 *
+	 * @param width the width
+	 * @param height the height
+	 * @param depth the depth
+	 */
 	public WorldBuilder(int width, int height, int depth) {
 		this.width = width;
 		this.height = height;
@@ -21,10 +44,20 @@ public class WorldBuilder {
 		this.nextRegion = 1;
 	}
 
+	/**
+	 * Builds the.
+	 *
+	 * @return the world
+	 */
 	public World build() {
 		return new World(tiles);
 	}
 
+	/**
+	 * Randomize tiles.
+	 *
+	 * @return the world builder
+	 */
 	private WorldBuilder randomizeTiles() {
 		for (int x = 0; x < width; x++) {
 			for (int y = 0; y < height; y++) {
@@ -36,37 +69,52 @@ public class WorldBuilder {
 		return this;
 	}
 
+	/**
+	 * Smooth.
+	 *
+	 * @param times the times
+	 * @return the world builder
+	 */
 	private WorldBuilder smooth(int times) {
 		Tile[][][] tiles2 = new Tile[width][height][depth];
 		for (int time = 0; time < times; time++) {
 
-			for (int x = 0; x < width; x++) {
-				for (int y = 0; y < height; y++) {
-					for (int z = 0; z < depth; z++) {
-						int floors = 0;
-						int rocks = 0;
-	
-						for (int ox = -1; ox < 2; ox++) {
-							for (int oy = -1; oy < 2; oy++) {
-								if (x + ox < 0 || x + ox >= width || y + oy < 0
-										|| y + oy >= height)
-									continue;
-	
-								if (tiles[x + ox][y + oy][z] == Tile.FLOOR)
-									floors++;
-								else
-									rocks++;
-							}
-						}
-						tiles2[x][y][z] = floors >= rocks ? Tile.FLOOR : Tile.WALL;
-					}
-				}
-			}
+			loopFor1(tiles2);
 			tiles = tiles2;
 		}
 		return this;
 	}
+
+	private void loopFor1(Tile[][][] tiles2) {
+		for (int x = 0; x < width; x++) {
+			for (int y = 0; y < height; y++) {
+				for (int z = 0; z < depth; z++) {
+					int floors = 0;
+					int rocks = 0;
+
+					for (int ox = -1; ox < 2; ox++) {
+						for (int oy = -1; oy < 2; oy++) {
+							if (x + ox < 0 || x + ox >= width || y + oy < 0
+									|| y + oy >= height)
+								continue;
+
+							if (tiles[x + ox][y + oy][z] == Tile.FLOOR)
+								floors++;
+							else
+								rocks++;
+						}
+					}
+					tiles2[x][y][z] = floors >= rocks ? Tile.FLOOR : Tile.WALL;
+				}
+			}
+		}
+	}
 	
+	/**
+	 * Creates the regions.
+	 *
+	 * @return the world builder
+	 */
 	private WorldBuilder createRegions(){
 		regions = new int[width][height][depth];
 		
@@ -85,6 +133,12 @@ public class WorldBuilder {
 		return this;
 	}
 	
+	/**
+	 * Removes the region.
+	 *
+	 * @param region the region
+	 * @param z the z
+	 */
 	private void removeRegion(int region, int z){
 		for (int x = 0; x < width; x++){
 			for (int y = 0; y < height; y++){
@@ -96,6 +150,15 @@ public class WorldBuilder {
 		}
 	}
 	
+	/**
+	 * Fill region.
+	 *
+	 * @param region the region
+	 * @param x the x
+	 * @param y the y
+	 * @param z the z
+	 * @return the int
+	 */
 	private int fillRegion(int region, int x, int y, int z) {
 		int size = 1;
 		ArrayList<Point> open = new ArrayList<Point>();
@@ -105,22 +168,32 @@ public class WorldBuilder {
 		while (!open.isEmpty()){
 			Point p = open.remove(0);
 
-			for (Point neighbor : p.neighbors8()){
-				if (neighbor.x < 0 || neighbor.y < 0 || neighbor.x >= width || neighbor.y >= height)
-					continue;
-				
-				if (regions[neighbor.x][neighbor.y][neighbor.z] > 0
-						|| tiles[neighbor.x][neighbor.y][neighbor.z] == Tile.WALL)
-					continue;
+			size = loopFor2(region, size, open, p);
+		}
+		return size;
+	}
 
-				size++;
-				regions[neighbor.x][neighbor.y][neighbor.z] = region;
-				open.add(neighbor);
-			}
+	private int loopFor2(int region, int size, ArrayList<Point> open, Point p) {
+		for (Point neighbor : p.neighbors8()){
+			if (neighbor.x < 0 || neighbor.y < 0 || neighbor.x >= width || neighbor.y >= height)
+				continue;
+			
+			if (regions[neighbor.x][neighbor.y][neighbor.z] > 0
+					|| tiles[neighbor.x][neighbor.y][neighbor.z] == Tile.WALL)
+				continue;
+
+			size++;
+			regions[neighbor.x][neighbor.y][neighbor.z] = region;
+			open.add(neighbor);
 		}
 		return size;
 	}
 	
+	/**
+	 * Connect regions.
+	 *
+	 * @return the world builder
+	 */
 	public WorldBuilder connectRegions(){
 		for (int z = 0; z < depth-1; z++){
 			connectRegionsDown(z);
@@ -128,22 +201,38 @@ public class WorldBuilder {
 		return this;
 	}
 	
+	/**
+	 * Connect regions down.
+	 *
+	 * @param z the z
+	 */
 	private void connectRegionsDown(int z){
 		List<Integer> connected = new ArrayList<Integer>();
 		
 		for (int x = 0; x < width; x++){
-			for (int y = 0; y < height; y++){
-				int r = regions[x][y][z] * 1000 + regions[x][y][z+1];
-				if (tiles[x][y][z] == Tile.FLOOR
-						&& tiles[x][y][z+1] == Tile.FLOOR
-						&& !connected.contains(r)){
-					connected.add(r);
-					connectRegionsDown(z, regions[x][y][z], regions[x][y][z+1]);
-				}
+			loopFor2(z, connected, x);
+		}
+	}
+
+	private void loopFor2(int z, List<Integer> connected, int x) {
+		for (int y = 0; y < height; y++){
+			int r = regions[x][y][z] * 1000 + regions[x][y][z+1];
+			if (tiles[x][y][z] == Tile.FLOOR
+					&& tiles[x][y][z+1] == Tile.FLOOR
+					&& !connected.contains(r)){
+				connected.add(r);
+				connectRegionsDown(z, regions[x][y][z], regions[x][y][z+1]);
 			}
 		}
 	}
 	
+	/**
+	 * Connect regions down.
+	 *
+	 * @param z the z
+	 * @param r1 the r 1
+	 * @param r2 the r 2
+	 */
 	private void connectRegionsDown(int z, int r1, int r2){
 		List<Point> candidates = findRegionOverlaps(z, r1, r2);
 		
@@ -157,6 +246,14 @@ public class WorldBuilder {
 		while (candidates.size() / stairs > 250);
 	}
 
+	/**
+	 * Find region overlaps.
+	 *
+	 * @param z the z
+	 * @param r1 the r 1
+	 * @param r2 the r 2
+	 * @return the list
+	 */
 	public List<Point> findRegionOverlaps(int z, int r1, int r2) {
 		ArrayList<Point> candidates = new ArrayList<Point>();
 		
@@ -175,6 +272,11 @@ public class WorldBuilder {
 		return candidates;
 	}
 	
+	/**
+	 * Adds the exit stairs.
+	 *
+	 * @return the world builder
+	 */
 	private WorldBuilder addExitStairs() {
 		int x = -1;
 		int y = -1;
@@ -189,6 +291,11 @@ public class WorldBuilder {
 		return this;
 	}
 
+	/**
+	 * Make caves.
+	 *
+	 * @return the world builder
+	 */
 	public WorldBuilder makeCaves() {
 		return randomizeTiles()
 				.smooth(8)
